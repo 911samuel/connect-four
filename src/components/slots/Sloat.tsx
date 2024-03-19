1624f1003d58ee7d9513bee32319287d5d5fb69b
911samuel <abayizeraeaz@gmail.com>
2024-03-19 17:24:16 +0200

    game functionalities

--- src/components/slots/Sloat.tsx
+++ src/components/slots/Sloat.tsx
@@ -1,23 +1,49 @@
 import React from "react";
 import red from "../../assets/counter-red-large.svg";
 import yellow from "../../assets/counter-yellow-large.svg";
-import './Slot.css'
+import "./Slot.css";
 
 interface SlotProps {
   ch: string | null;
   y: number;
   x: number;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+  onClick: (col: number) => void;
+  onClickSlot: (y: number, x: number) => void;
 }
 
-const Slot: React.FC<SlotProps> = ({ ch, y, x }) => {
+const imageStyle: React.CSSProperties = {
+  width: "100%", 
+  height: "auto",
+};
+
+const Slot: React.FC<SlotProps> = ({
+  ch,
+  y,
+  x,
+  onMouseEnter,
+  onMouseLeave,
+  onClick,
+  onClickSlot,
+}) => {
   return (
-    <div className="slot" data-x={x} data-y={y}>
+    <div
+      className="slot"
+      data-x={x}
+      data-y={y}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={() => {
+        onClick(y);
+        onClickSlot(y, x);
+      }}
+    >
       {ch && (
         <img
           src={ch === "X" ? red : yellow}
           alt={ch === "X" ? "Red" : "Yellow Counter"}
-          width="100%"
-          height="100%"
+          style={imageStyle}
         />
       )}
     </div>
